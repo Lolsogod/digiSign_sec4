e440609d5ca8a966319e19c2ef77410416a03b25
DIO <46161640+Lolsogod@users.noreply.github.com>
2023-03-31 15:55:37 +0400

    fin rsa

--- index.js
+++ index.js
@@ -33,39 +33,56 @@ const p = __importStar(require("path"));
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
 const crypto_1 = __importDefault(require("crypto"));
+let file;
+let path = reader.question("File path: ");
+let fileName = p.basename(path);
+file = fs_1.default.readFileSync(path);
 const sign = () => {
-    let file;
-    let path = reader.question("path: ");
-    let fileName = p.basename(path);
-    file = fs_1.default.readFileSync(path);
-    if (file) {
-        let { publicKey, privateKey } = crypto_1.default.generateKeyPairSync('rsa', {
-            modulusLength: 2048,
-            publicKeyEncoding: {
-                type: 'spki',
-                format: 'pem',
-            },
-            privateKeyEncoding: {
-                type: 'pkcs8',
-                format: 'pem'
-            }
-        });
-        console.log(publicKey);
-        const sign = crypto_1.default.createSign('sha256');
-        sign.update(file);
-        sign.end();
-        const signature = sign.sign(privateKey);
-        fs_1.default.writeFileSync(`${__dirname}/files/${fileName}.sig`, signature, {
-            flag: "w"
-        });
-        fs_1.default.writeFileSync(`${__dirname}/files/${fileName}.key`, publicKey, {
-            flag: "w"
-        });
-        const verify = crypto_1.default.createVerify('sha256');
-        verify.update(file);
-        verify.end();
-        let result = verify.verify(publicKey, signature);
-        console.log(result);
-    }
+    let { publicKey, privateKey } = crypto_1.default.generateKeyPairSync('rsa', {
+        modulusLength: 2048,
+        publicKeyEncoding: {
+            type: 'spki',
+            format: 'pem',
+        },
+        privateKeyEncoding: {
+            type: 'pkcs8',
+            format: 'pem'
+        }
+    });
+    console.log(publicKey);
+    const sign = crypto_1.default.createSign('sha256');
+    sign.update(file);
+    sign.end();
+    const signature = sign.sign(privateKey);
+    fs_1.default.writeFileSync(`${__dirname}/files/${fileName}.sig`, signature, {
+        flag: "w"
+    });
+    fs_1.default.writeFileSync(`${__dirname}/files/${fileName}.key`, publicKey, {
+        flag: "w"
+    });
 };
-sign();
+const verify = () => {
+    const verify = crypto_1.default.createVerify('sha256');
+    verify.update(file);
+    verify.end();
+    let signature;
+    let sigPath = reader.question("Signature path: ");
+    signature = fs_1.default.readFileSync(sigPath);
+    let publicKey;
+    let keyPath = reader.question("Public key path: ");
+    publicKey = fs_1.default.readFileSync(keyPath);
+    let result = verify.verify(publicKey, signature);
+    console.log(result);
+};
+let operation = reader.question("Sign or verify (s/v)?: ");
+console.log(operation);
+switch (operation) {
+    case "s":
+        sign();
+        break;
+    case "v":
+        verify();
+        break;
+    default:
+        break;
+}
--- index.ts
+++ index.ts
@@ -6,43 +6,59 @@ const app = express()
 app.use(express.json())
 import crypto from 'crypto'
 
+let file: any;
+let path = reader.question("File path: ");
+let fileName = p.basename(path);
+file = fs.readFileSync(path)
 
 const sign = () =>{
-    let file;
-    let path = reader.question("path: ");
-    let fileName = p.basename(path);
-    file = fs.readFileSync(path)
-    if (file){
-        let {publicKey, privateKey} = crypto.generateKeyPairSync('rsa', {
-            modulusLength: 2048,
-            publicKeyEncoding: {
-                type: 'spki',
-                format: 'pem',
-            },
-            privateKeyEncoding: {
-                type: 'pkcs8',
-                format: 'pem'
-            }
-        })
-        console.log(publicKey)
-        const sign = crypto.createSign('sha256')
-        sign.update(file)
-        sign.end()
-        const signature = sign.sign(privateKey)
-        fs.writeFileSync(`${__dirname}/files/${fileName}.sig`, signature, {
-            flag: "w"
-        });
-        fs.writeFileSync(`${__dirname}/files/${fileName}.key`, publicKey, {
-            flag: "w"
-        });
-        
-        const verify = crypto.createVerify('sha256')
-        verify.update(file)
-        verify.end()
-
-        let result = verify.verify(publicKey, signature)
-        console.log(result)
-    }
+    let {publicKey, privateKey} = crypto.generateKeyPairSync('rsa', {
+        modulusLength: 2048,
+        publicKeyEncoding: {
+            type: 'spki',
+            format: 'pem',
+        },
+        privateKeyEncoding: {
+            type: 'pkcs8',
+            format: 'pem'
+        }
+    })
+    console.log(publicKey)
+    const sign = crypto.createSign('sha256')
+    sign.update(file)
+    sign.end()
+    const signature = sign.sign(privateKey)
+    fs.writeFileSync(`${__dirname}/files/${fileName}.sig`, signature, {
+        flag: "w"
+    });
+    fs.writeFileSync(`${__dirname}/files/${fileName}.key`, publicKey, {
+        flag: "w"
+    });
 }
 
-sign()
+const verify = () =>{
+    const verify = crypto.createVerify('sha256')
+        verify.update(file)
+        verify.end()
+        let signature: any;
+        let sigPath = reader.question("Signature path: ");
+        signature = fs.readFileSync(sigPath)
+        let publicKey: any;
+        let keyPath = reader.question("Public key path: ");
+        publicKey = fs.readFileSync(keyPath)
+        let result = verify.verify(publicKey, signature)
+        console.log(result)
+}
+
+let operation = reader.question("Sign or verify (s/v)?: ");
+console.log(operation)
+switch (operation) {
+    case "s":
+        sign()
+        break;
+    case "v":
+        verify()
+        break
+    default:
+        break;
+}
